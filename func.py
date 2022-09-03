e352a7c6102f606d583ab29843fd14c125b97535
27102003 <112692662+27102003@users.noreply.github.com>
2022-09-03 14:28:24 +0530

    Add files via upload

---
+++ func.py
@@ -0,0 +1,15 @@
+def sum(a,b):
+    print("Sum is: "+str(a+b))
+def mul(a,b):
+    print("Multiply is: "+str(a*b))
+def sub(a,b):
+    print("subtraction is: "+str(a-b))
+def div(a,b):
+    print("Division is: "+str(a/b))
+
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+sum(a,b)
+mul(a,b)
+sub(a,b)
+div(a,b)
